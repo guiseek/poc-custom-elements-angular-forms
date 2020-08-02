f563bc608f9b61bc7f9317e9104885e3962d8f49
Guilherme Siquinelli (guiseek) <guiseek@gmail.com>
2020-08-02 05:17:48 -0300

    test

--- libs/ui/form/angular/src/lib/adapters/input-hoist.directive.spec.ts
+++ libs/ui/form/angular/src/lib/adapters/input-hoist.directive.spec.ts
@@ -1,8 +1,28 @@
 import { InputHoistDirective } from './input-hoist.directive';
+import { createMockFor } from '@nxc/util/testing';
+import { ControlContainer, AbstractControl } from '@angular/forms';
+import { ElementRef } from '@angular/core';
 
+class Container extends ControlContainer {
+  get control(): AbstractControl {
+    throw new Error('Method not implemented.');
+  }
+  constructor() {
+    super();
+  }
+}
 describe('InputHoistDirective', () => {
+  let directive: InputHoistDirective;
+
+  function createInputHoist({
+    containerMock = createMockFor(Container),
+    elementRefMock = ElementRef,
+  } = {}) {
+    return { containerMock, elementRefMock };
+  }
   it('should create an instance', () => {
-    const directive = new InputHoistDirective();
+    const { containerMock, elementRefMock } = createInputHoist();
+    directive = new InputHoistDirective(containerMock, elementRefMock as any);
     expect(directive).toBeTruthy();
   });
 });
